f496cbd712b51238a76cfaf5bf5b2877c37bd286
David Zoufalý <davidzoufaly@gmail.com>
2025-09-29 15:51:27 +0200

    feat: first workflow

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
+// src/index.ts
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-
-async function run(): Promise<void> {
+async function run() {
   try {
     const name = core.getInput("name", { required: true });
     const msg = `Hello, ${name}! From repo ${github.context.repo.owner}/${github.context.repo.repo} on ${github.context.eventName}.`;
     core.setOutput("message", msg);
-  } catch (err: any) {
+  } catch (err) {
     core.setFailed(err?.message ?? String(err));
   }
 }
-
 run();
+//# sourceMappingURL=index.js.map
